b70738b51b72224693c27feab30b6769c628e12c
adityaj358 <adityajiwane182000@gmail.com>
2024-10-03 17:47:59 +0530

    commit2

--- database.js
+++ database.js
@@ -1,4 +1,40 @@
-require("mongodb")
+const {MongoClient} = require("mongodb");
 
-const URI = 
-"mongodb+srv://adityajiwane182000:SS321PffaZQsARD8@cluster0.rbi2w.mongodb.net/"
+const URL = 
+"mongodb+srv://adityajiwane182000:SS321PffaZQsARD8@cluster0.rbi2w.mongodb.net/";
+
+const client = new MongoClient(URL);
+
+const dbName = 'HelloWorld';
+
+async function main(){
+
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection("User");
+
+    const insertData = {
+        "firstName" : "Suraj",
+        "lastname" : "Sao",
+        "Age" : 24,
+        "Location" : "Chandrapur",
+        "Mobile" : "9637268059"
+    };
+
+    const insertResult = await collection.insertMany([insertData]);
+    console.log("inserte documents =>", insertResult);
+
+    //read from compass
+    const findResult = await collection.find({}).toArray();
+    console.log("found docs =>" ,findResult);
+
+    const countResult = await collection.countDocuments({});
+    console.log("count of docs in user collection is => ", countResult);
+
+    return "done";
+}
+main()
+.then(console.log)
+.catch(console.error)
+.finally(() => client.close())
